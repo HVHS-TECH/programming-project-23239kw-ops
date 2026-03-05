d762436edf6329b6b1755973505ef860454237f5
2323kw-ops <23239kw@hvhs.school.nz>
2026-03-06 09:58:03 +1300

    6 Mar

--- birdComing.js
+++ birdComing.js
@@ -17,17 +17,31 @@ function preload() {
 
   power = 0;
 
+  playerProjectile.vel.y = 0;
+
 }
 
 
 function setup() {
 	console.log("setup: ");
-	cnv = new Canvas(windowWidth, windowHeight);
+	cnv = new Canvas(1920, 940);
   player = new Sprite(100, height-100, 200, 50, 'd');
-  imgPlayer.resize(200, 50);
+  imgPlayer.resize(200, 30);
   player.image = (imgPlayer);
 
   world.gravity = 10;
+
+  for (i = 0; i < 100; i++) {
+    alien = new Sprite(width / 2, height / 2, 50, 50, 'd');
+    alien.vel.x = random();
+    alien.vel.y = random();
+    alien.bounciness = 1;
+    alien.friction = 5;
+	  alien.color = 'red';
+    const VELARRAY = [-1, 1];
+	  alienGroup.add(alien);
+
+	}
 
 }
 
@@ -58,27 +72,30 @@ function draw() {
 
 	}
 
-  if (mouse.presses()) {
+  if (mouse.pressing()) {
 
-    let radians = playerTilt * (Math.PI / 180);
-    let barrelLength = 100;
-
-    let spawnX = player.x + barrelLength * Math.cos(radians);
-    let spawnY = player.y + barrelLength * Math.sin(radians);
-
-    playerProjectile = new Sprite(spawnX, spawnY, 20, 'd');
-
-    playerProjectile.vel.x = power * Math.cos(radians);
-    playerProjectile.vel.y = power * Math.sin(radians);
-    playerProjectile.colour = ('black')
+    if (power < 100) {
+      power += 1;
+    }
 
   }
 
-    if (power <100) {
-      power += 1;
-    } else{
-      power = 0;
-    }
+  if (mouse.released()) {
+      let radians = playerTilt * (Math.PI / 180);
+      let barrelLength = 100;
+
+      let spawnX = player.x + barrelLength * Math.cos(radians);
+      let spawnY = player.y + barrelLength * Math.sin(radians);
+
+      playerProjectile = new Sprite(spawnX, spawnY, 20, 'd');
+
+      playerProjectile.vel.x = power * Math.cos(radians);
+      playerProjectile.vel.y = power * Math.sin(radians);
+      playerProjectile.colour = ('black')
+
+      power = 0
+  }
+
 }
 
 /*******************************************************/
